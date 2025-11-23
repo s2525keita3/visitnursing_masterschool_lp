96644a1891a31ba6909fa5e995a792f69a21e3c9
kigyo-lp-owner <s2525keita3@gmail.com>
2025-11-23 15:14:09 +0900

    Vite base path fix and final build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/visitnursing_masterschool_lp/', // ⬅️ この行が必須！
+  base: '/visitnursing_masterschool_lp/', 
   plugins: [react()],
 });
