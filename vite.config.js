22d1ca0f9ea78f8886e025567251ff17a019068b
kigyo-lp-owner <s2525keita3@gmail.com>
2025-11-23 15:57:34 +0900

    Final fix: Corrected defineConfig import error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-// vite.config.js の中身
+import { defineConfig } from 'vite'; // 
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/visitnursing_masterschool_lp/', //
+  base: '/visitnursing_masterschool_lp/', // 
   plugins: [react()],
 });
