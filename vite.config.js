d3a0646f4d3619dd107b33286777424e0f543c91
s2525keita3 <s2525keita3@gmail.com>
2025-11-26 23:47:27 +0900

    Vercel移行用に設定変更とdocs削除

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // GitHub Pagesでのパス指定を正しく行うための設定
   plugins: [react()],
-  build: {
-    outDir: 'docs', // ⭐ ここが重要！ 出力先を docs フォルダに指定
-  }
+
+  // ▼▼▼ 以下の設定を削除またはコメントアウトしてください ▼▼▼
+  // base: './', 
+  // build: {
+  //   outDir: 'docs',
+  // },
+  // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 });
